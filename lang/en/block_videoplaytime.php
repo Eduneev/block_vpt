7261c15ade98bf3bf5a7aa3be950e0f143d818ff
Sanat Sharma <sanatcolumban@gmail.com>
2021-03-24 11:29:33 +0530

    language

---
+++ lang/en/block_videoplaytime.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+$string['pluginname'] = 'Video Play Time';
+$string['simplehtml'] = 'Video Play Time';
+$string['simplehtml:addinstance'] = 'Add a new User Available video playtime block';
+$string['simplehtml:myaddinstance'] = 'Add a new User Available video playtime block to the My Moodle page';
+$string['videoplaytime'] = 'Video Playtime';
+$string['maximumplaytime'] = 'Maximum Playtime';
+
+$string['allocation'] = 'Allocation';
+$string['utilised'] = 'Utilised';
+$string['available'] = 'Available';
+$string['noallocation'] = 'Nothing to display.';
